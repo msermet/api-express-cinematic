8cfe8d9e86632dca16acde58672cd2f62b9a65f5
sermetm <eargaerg@aergaerg.com>
2025-03-24 14:00:59 +0100

    pret pour demain

--- myapp/app.js
+++ myapp/app.js
@@ -25,6 +25,30 @@ app.get('/api/films', (req, res) => {
     });
 });
 
+// app.get(`/api/films/:id`, (req, res) => {
+//     const query =   'SELECT f.id,f.titre,f.realisateur,f.duree,DATE_FORMAT(f.date_sortie, "%Y-%m-%d") as date_sortie,f.affiche,f.genre_id,f.description,g.nom as genre_nom ' +
+//         '                   FROM films f ' +
+//         '                   INNER JOIN genres g on g.id=f.genre_id' +
+//         '                   WHERE f.genre_id=g.id' +
+//         '                   AND f.id=:id';
+//
+//     // probleme de récupération de l'id dans la requete
+//
+//
+//     connection.query(query, (erreur, resultats) => {
+//         if (erreur) {
+//             console.error('Erreur lors de la récupération des données :', erreur);
+//             res.status(500).json({ error: 'Erreur lors de la récupération des données' });
+//             return;
+//         }
+//
+//         res.json({
+//             status: erreur === null ? 'success' : 'error',
+//             data: resultats
+//         });
+//     });
+// });
+
 app.listen(port, () => {
-    console.log(`Lien de l'api : http://localhost:${port}`);
+    console.log(`Le lien api : http://localhost:${port}`);
 });
